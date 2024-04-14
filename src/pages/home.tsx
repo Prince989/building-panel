ce718ffd75f14b4dab9a817d689b4d440f13fb88
Alireza <alireza1377aj@gmail.com>
2024-04-14 21:49:00 +0330

    Initial Commit

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -2,9 +2,17 @@ import React from 'react'
 import Header from '../Components/Header'
 
 export default function HomePage() {
-  return (
-    <div>
-        <Header />
-    </div>
-  )
+
+    if (window.innerWidth < 1000)
+        return (
+            <div className='text-center text-2xl font-semibold'>
+                لطفا از طریق کامپیوتر وارد شوید
+            </div>
+        )
+
+    return (
+        <div>
+            <Header />
+        </div>
+    )
 }

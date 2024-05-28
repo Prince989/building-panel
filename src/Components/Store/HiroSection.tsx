a7832d19453af6d4816f2742cba0f93f4fb93c8e
Alireza <alireza1377aj@gmail.com>
2024-05-28 18:35:59 +0330

    Hiro section updated

--- src/Components/Store/HiroSection.tsx
+++ src/Components/Store/HiroSection.tsx
@@ -6,10 +6,10 @@ export default function HiroSection() {
     <div dir='rtl' className='h-[479px] bg-[#D2C9C0] pr-[88px] pt-[107px] w-full relative'>
       <img src='/assets/dummy-2.png' className='absolute h-full left-0 bottom-0 z-[50]' />
       <h1 className='text-[48px] font-semibold'>
-        اقساطی صاحبخانه شوید...
+        اینجا محل تبلیغات شماست
       </h1>
-      <p className='mt-[20px]'>
-        دات ملک بهترین مشاور خرید ، فروش و سرمایه گذاری  در حوزه املاک
+      <p className='mt-[20px] text-[29px] max-w-[542px] font-medium text-[#333333]'>
+        ici اولین پلتفرم تخصصی ، در حوزه صنعت ساختمان
       </p>
       <Button variant='contained' className='flex mt-[56px] font-medium px-[54px] gap-[12px] justify-center items-center text-[26px] py-[12px]'>
         <img src='/assets/call.svg' className='' />
